e2fa48bb382a6047c0916baac0c01f6d7053a966
Tristan <thrawst88@gmail.com>
2019-02-08 16:24:18 +0100

    fix better matches threshold, fix arg passing bug

--- src/evaluator.py
+++ src/evaluator.py
@@ -2,7 +2,6 @@ import argparse
 import numpy as np
 import pandas as pd
 
-from time import time
 from imageio import imread, imwrite
 
 from im_split import im_split
@@ -11,18 +10,20 @@ from feature_based.AKAZE import AKAZE
 
 def eval_method(image, original, method, **kwargs):
   blocks = im_split(image, **kwargs)
-  start = time()
-  stitched = method(blocks)
-  duration = (time() - start) / (len(blocks) - 1)
-  result = evaluation2(stitched, original)
-  print("t: %0.2f, a: %0.2f" % (duration, result))
+  stitched, duration = method(blocks)
+  result = evaluation(stitched, original)
   return (duration, result)
 
 
 def eval_param(image, method, param, data_range):
-  table = pd.DataFrame(columns=['noise', 'time', cname])
+  cname = method.__name__
+  table = pd.DataFrame(columns=[param, 'time', cname])
+  orig = imread(image)
+  print('%s evaluation with %s' % (param, method.__name__))
   for val in data_range:
-    duration, result = eval_method(image, orig, method, noise=val)
+    kw = { param: val }
+    duration, result = eval_method(image, orig, method, **kw)
+    print("param: %0.02f, t: %0.2f, a: %0.2f" % (val, duration, result))
     table = table.append(
       {param: val, 'time': duration, cname: result}, ignore_index=True)
   return table
@@ -30,15 +31,12 @@ def eval_param(image, method, param, data_range):
 
 def run_eval(image, method, noise=False, rotation=False, overlap=False):
   # if no arguments are provided all are run.
-  if not (noise and rotation and overlap):
-    noise = True
+  if not noise and not rotation and not overlap:
+    noise    = True
     rotation = True
-    overlap = True
+    overlap  = True
 
-  cname = method.__name__
-  orig = imread(image)
   out = []
-
   if noise:
     out.append(eval_param(image, method, 'noise', [d / 100 for d in range(0,11,2)]))
 
@@ -46,7 +44,7 @@ def run_eval(image, method, noise=False, rotation=False, overlap=False):
     out.append(eval_param(image, method, 'rotation', range(0,181,45)))
 
   if overlap:
-    out.append(eval_param(image, method, 'overlap', [o/100 for o in range(0, 50, 5)]))
+    out.append(eval_param(image, method, 'overlap', [o/100 for o in range(5, 51, 5)]))
 
   return out
 
@@ -95,12 +93,11 @@ def evaluation2(stitched, original):
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Run evaluations with a method')
-  parser.add_argument('-noise', metavar='n', type=str, help='run noise evaluations')
-  parser.add_argument('-rotation', metavar='r', type=str, help='run rotation evaluations')
-  parser.add_argument('-overlap', metavar='o', type=str, help='run overlap evaluations')
+  parser.add_argument('-noise', action='store_true', help='run noise evaluations')
+  parser.add_argument('-rotation', action='store_true', help='run rotation evaluations')
+  parser.add_argument('-overlap', action='store_true', help='run overlap evaluations')
   args = parser.parse_args()
-  kw = {'noise': args.n, 'rotation': args.r, 'overlap': args.o }
-  print(kw)
+  kw = vars(args)
   x = run_eval('../data/S2_Img_003.00.tif', AKAZE, **kw)
   for t in x:
     print(t)
--- src/feature_based/AKAZE.py
+++ src/feature_based/AKAZE.py
@@ -7,6 +7,7 @@ import matplotlib.lines as mlines
 from collections import deque
 from imageio import imread, imwrite
 from math import floor
+from time import time
 
 def get_akaze_keypoints(im):
     akaze = cv2.AKAZE_create()
@@ -17,14 +18,16 @@ def AKAZE(blocks):
     feature_points = []
     bf = cv2.BFMatcher()
     queue = deque(blocks)
-    base = queue[0]
-    queue.popleft()
+    base = queue.popleft()
     ind = 0
+    total_time = 0
     while len(queue) > 0:
+        # load images
         im1 = base
-        current_block = queue[0]
-        queue.popleft()
-        im2 = current_block
+        im2 = queue.popleft()
+
+        # start timer
+        start = time()
 
         # Find feature points
         (kp_1, desc_1) = get_akaze_keypoints(im1)
@@ -35,28 +38,34 @@ def AKAZE(blocks):
         matches = sorted(matches, key = lambda x:x[0].distance)
         better_matches = [ m[0] for m in matches if m[0].distance < 0.75 * m[1].distance]
 
-        if len(better_matches) == 0:
+        # make sure we have enough matches.
+        if len(better_matches) < 10:
             # cache feature points
-            print('could not find a match')
+            print('could not find enough matches')
             continue
+
         # Warp the second image to best match the first.
         src_pts = np.float32([ kp_2[m.trainIdx].pt for m in better_matches ]).reshape(-1,1,2)
         dst_pts = np.float32([ kp_1[m.queryIdx].pt for m in better_matches ]).reshape(-1,1,2)
 
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
         h,w = im1.shape
+
         # use the homography offsets to determine the size for the stitched image
         x_offset, y_offset = M[0:2, 2]
         new_size = (int(max(w, w+x_offset)), int(max(h, h+y_offset)))
+
         # warp!
         warped = cv2.warpAffine(im2, M[:2,:3], new_size, flags=cv2.INTER_CUBIC)
         print('stitched %d with %d' % (ind, ind + 1))
         ind += 1
         base = paste(im1, warped)
+        total_time += time() - start
 
     base = crop_zeros(base)
     imwrite('../data/stitched.tif', base)
-    return base
+    average_time = total_time / (len(blocks) - 1)
+    return (base, average_time)
 
 
 def concat_images(imga, imgb):
